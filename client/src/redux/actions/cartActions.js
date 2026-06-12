f6a62d0c577489172d4a9718c9e9ae13956efcb9
Abel <abelreji25@users.noreply.github.com>
2026-06-12 22:47:29 +0530

    Fix Redux actions URL to point to Render API

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,9 +1,11 @@
 import * as actionTypes from '../constants/cartConstants';
 import axios from 'axios';
 
+const URL = 'https://flipkartclone-unnx.onrender.com';
+
 export const addToCart = (id, quantity) => async (dispatch) => {
     try { 
-        const { data } = await axios.get(`/product/${id}`);
+        const { data } = await axios.get(`${URL}/product/${id}`);
 
         dispatch({ type: actionTypes.ADD_TO_CART, payload: { ...data, quantity } });
 
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,9 +1,11 @@
 import * as actionTypes from '../constants/productConstant';
 import axios from 'axios';
 
+const URL = 'https://flipkartclone-unnx.onrender.com';
+
 export const getProducts = () => async (dispatch) => {
     try {
-        const { data } = await axios.get(`/products`);
+        const { data } = await axios.get(`${URL}/products`);
         dispatch({ type: actionTypes.GET_PRODUCTS_SUCCESS, payload: data });
 
     } catch (error) {
@@ -14,7 +16,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
-        const { data } = await axios.get(`/product/${id}`);
+        const { data } = await axios.get(`${URL}/product/${id}`);
         
         dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
 
